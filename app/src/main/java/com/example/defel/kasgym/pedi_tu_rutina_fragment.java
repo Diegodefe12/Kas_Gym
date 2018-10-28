0360771dd78cda530bfb3e81d46e1b2202d62072
Diego <defelippediego@hotmail.com>
2018-10-28 00:30:59 -0300

    Nuevo Fragement

---
+++ app/src/main/java/com/example/defel/kasgym/pedi_tu_rutina_fragment.java
@@ -0,0 +1,29 @@
+package com.example.defel.kasgym;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class pedi_tu_rutina_fragment extends Fragment {
+
+
+    public pedi_tu_rutina_fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pedi_tu_rutina_fragment, container, false);
+    }
+
+}
